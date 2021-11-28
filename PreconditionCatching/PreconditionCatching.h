fa63ebdd04008f03c94c04c1bd53242f19cb669b
brennanMKE <277419+brennanMKE@users.noreply.github.com>
2021-11-28 13:07:46 -0800

    fixes build to work with Hela

--- PreconditionCatching/PreconditionCatching.h
+++ PreconditionCatching/PreconditionCatching.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double PreconditionCatchingVersionNumber;
 FOUNDATION_EXPORT const unsigned char PreconditionCatchingVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PreconditionCatching/PublicHeader.h>
-
-
